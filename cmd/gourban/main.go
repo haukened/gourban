a03ee8b7d8b7b061381d58768ca352c3eee9f825
David Haukeness <david@hauken.us>
2019-11-12 16:21:50 -0700

    updated to take query string as command line argument

--- cmd/gourban/main.go
+++ cmd/gourban/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
 	"github.com/haukened/gourban"
 )
 
 func main() {
-	test, err := gourban.Query("yeet")
+	if len(os.Args) <= 1 {
+		fmt.Fprint(os.Stderr, "Missing required query string.\n")
+		os.Exit(1)
+	}
+	queryString := strings.Join(os.Args[1:], " ")
+	test, err := gourban.Query(queryString)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%s: %s\n", test[0].Word, test[0].Definition)
+	fmt.Printf("[%s]: %s\n", test[0].Word, test[0].Definition)
 }
